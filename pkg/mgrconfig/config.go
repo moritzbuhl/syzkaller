655e912426eb2a9744ed24a3c9d09654161d07f5
Moritz Buhl <moritz.buhl@gmail.com>
2025-07-24 11:14:18 +0200

    doc: remove stray double quote from comment

--- pkg/mgrconfig/config.go
+++ pkg/mgrconfig/config.go
@@ -253,8 +253,8 @@ type Experimental struct {
 	// If FocusAreas is non-empty, by default all kernel code not covered by any filter will be ignored.
 	// To focus fuzzing on some areas, but to consider the rest of the code as well, add a record
 	// with an empty Filter, but non-empty weight.
-	// E.g. "focus_areas": [ {"filter": {"files": ["^net"]}, "weight": 10.0}, {"weight": 1.0"} ].
-	FocusAreas []FocusArea `json:"focus_areas,omitempty"`
+	// E.g. "focus_areas": [ {"filter": {"files": ["^net"]}, "weight": 10.0}, {"weight": 1.0} ].
+	FocusAreas []FocusArea `json:"focus_areas,omitempty"`0
 }
 
 type FocusArea struct {
